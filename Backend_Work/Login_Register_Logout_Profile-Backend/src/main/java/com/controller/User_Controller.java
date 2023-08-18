cc9435ec72e3bcb6fc3a913d01d4c88070029cfc
sankha-mondal <99641675+sankha-mondal@users.noreply.github.com>
2023-08-19 00:17:34 +0530

    [Login Register Logout Profile] in Backend

--- Backend_Work/Login_Register_Logout_Profile-Backend/src/main/java/com/controller/User_Controller.java
+++ Backend_Work/Login_Register_Logout_Profile-Backend/src/main/java/com/controller/User_Controller.java
@@ -1,7 +1,6 @@
 package com.controller;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
