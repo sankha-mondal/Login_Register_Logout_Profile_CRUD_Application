0cbdaf02a7633f3e0ad7fd206f75464916f7273f
sankha-mondal <99641675+sankha-mondal@users.noreply.github.com>
2023-09-07 17:13:11 +0530

    [Login Register Logout Profile] in Backend

--- Backend_Work/Login_Register_Logout_Profile-Backend/src/main/java/com/repository/User_Repository.java
+++ Backend_Work/Login_Register_Logout_Profile-Backend/src/main/java/com/repository/User_Repository.java
@@ -1,6 +1,6 @@
 package com.repository;
 
-
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import com.entity.User;
